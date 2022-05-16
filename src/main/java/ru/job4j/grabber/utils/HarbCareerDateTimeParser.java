4e53c3b4029f4468ea3087a83dcad501c573eaba
Michael Lobov <michael.lobov13@gmail.com>
2022-05-16 18:51:22 +0300

    2.1. Преобразование даты [#289476 #301326]

--- src/main/java/ru/job4j/grabber/utils/HarbCareerDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/HarbCareerDateTimeParser.java
@@ -1,16 +1,11 @@
 package ru.job4j.grabber.utils;
 
 import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.time.ZonedDateTime;
 
 public class HarbCareerDateTimeParser implements DateTimeParser {
     @Override
     public LocalDateTime parse(String parse) {
-        List<Integer> date = Arrays.stream(parse.split("\\+")[0].split("\\D"))
-            .map(Integer::parseInt)
-            .collect(Collectors.toList());
-        return LocalDateTime.of(date.get(0), date.get(1), date.get(2), date.get(3), date.get(4), date.get(5));
+        return ZonedDateTime.parse(parse).toLocalDateTime();
     }
 }
