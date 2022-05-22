7f0b09896c773b0453aa6fca936b19275a701083
Michael Lobov <michael.lobov13@gmail.com>
2022-05-22 13:04:01 +0300

    Revert "2.4. SqlRuParse [#285213 #303409]"
    This reverts commit 0c3ae330

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -5,9 +5,8 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import ru.job4j.grabber.utils.DateTimeParser;
+import ru.job4j.grabber.utils.HarbCareerDateTimeParser;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -16,9 +15,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class HabrCareerParse implements Parse {
-    private static final int PAGE_LIMIT = 1;
     private final DateTimeParser dateTimeParser;
-    private static final Logger LOG = LoggerFactory.getLogger(HabrCareerParse.class.getName());
 
     public HabrCareerParse(DateTimeParser dateTimeParser) {
         this.dateTimeParser = dateTimeParser;
@@ -27,27 +24,25 @@ public class HabrCareerParse implements Parse {
     @Override
     public List<Post> list(String link) {
         List<Post> posts = new ArrayList<>();
-        for (int page = 1; page <= PAGE_LIMIT; page++) {
+        for (int page = 1; page < 6; page++) {
             try {
                 Connection connection = Jsoup.connect(link + "?page=" + page);
                 Document document = connection.get();
                 Elements rows = document.select(".vacancy-card__inner");
-                rows.forEach(row -> posts.add(vacancyParse(row)));
+                rows.forEach(row -> {
+                    Element titleElement = row.select(".vacancy-card__title").first();
+                    Element linkElement = titleElement.child(0);
+                    String vacancyName = titleElement.text();
+                    String vacancyLink = String.format("%s%s", getSourceLink(link), linkElement.attr("href"));
+                    Element dateElement = row.select(".vacancy-card__date").first();
+                    String date = dateElement.child(0).attr("datetime");
+                    posts.add(new Post(vacancyName, vacancyLink, retrieveDescription(vacancyLink), dateTimeParser.parse(date)));
+                });
             } catch (IOException e) {
-                LOG.error(e.getMessage());
+                e.printStackTrace();
             }
         }
         return posts;
-    }
-
-    private Post vacancyParse(Element element) {
-        Element titleElement = element.select(".vacancy-card__title").first();
-        Element linkElement = titleElement.child(0);
-        String vacancyName = titleElement.text();
-        String vacancyLink = String.format("%s%s", getSourceLink(element.baseUri()), linkElement.attr("href"));
-        Element dateElement = element.select(".vacancy-card__date").first();
-        String date = dateElement.child(0).attr("datetime");
-        return new Post(vacancyName, vacancyLink, retrieveDescription(vacancyLink), dateTimeParser.parse(date));
     }
 
     private String retrieveDescription(String link) {
@@ -56,7 +51,7 @@ public class HabrCareerParse implements Parse {
             Document document = Jsoup.connect(link).get();
             description = document.select(".style-ugc").text();
         } catch (IOException e) {
-            LOG.error(e.getMessage());
+            e.printStackTrace();
         }
         return description;
     }
@@ -69,5 +64,10 @@ public class HabrCareerParse implements Parse {
             sourceLink = matcher.group();
         }
         return sourceLink;
+    }
+
+    public static void main(String[] args) {
+        HabrCareerParse h = new HabrCareerParse(new HarbCareerDateTimeParser());
+        h.list("https://career.habr.com/vacancies/java_developer");
     }
 }
