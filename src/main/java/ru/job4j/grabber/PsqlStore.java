c7c1b2fed41da506aeaaff74221c6c9ad618f5b9
Michael Lobov <michael.lobov13@gmail.com>
2022-05-27 18:28:52 +0300

    5. PsqlStore [#285209 #305727]

--- src/main/java/ru/job4j/grabber/PsqlStore.java
+++ src/main/java/ru/job4j/grabber/PsqlStore.java
@@ -30,7 +30,10 @@ public class PsqlStore implements Store, AutoCloseable {
     @Override
     public void save(Post post) {
         try (PreparedStatement statement = cnn.prepareStatement(
-            "insert into post(name, text, link, created) values(?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS
+            "insert into post(name, text, link, created) values(?, ?, ?, ?) on conflict (link) do update set"
+                + " name =  EXCLUDED.name, text =  EXCLUDED.text,"
+                + " link =  EXCLUDED.link, created =  EXCLUDED.created",
+            Statement.RETURN_GENERATED_KEYS
         )) {
             statement.setString(1, post.getTitle());
             statement.setString(2, post.getDescription());
